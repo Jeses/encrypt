cf1da44a5fc91ad88857de694a2bc66216eb0903
zhengCai <zhengcai@ziyun120.com>
2024-06-21 18:24:17 +0800

    修改签名方法逻辑

--- src/Encrypt.php
+++ src/Encrypt.php
@@ -186,7 +186,7 @@ class Encrypt
 	/**
 	 * Desc: 创建签名
 	 * @param array $data 数据
-	 * @param string $appKey  公钥
+	 * @param string $appKey 公钥
 	 * @param string $appSecret 私钥
 	 * @return array
 	 * User: 青山有木
@@ -195,18 +195,27 @@ class Encrypt
 	 */
 	public static function createSign(array $data, string $appKey, string $appSecret)
 	{
-		if (isset($data['_sign']))
+		if (isset($data['_sign'])) {
 			unset($data['_sign']);
+		}
 
 		$data['_appKey'] = $appKey;
 		$data['_timestamp'] = time();
 		$signStr = $appSecret;
 		ksort($data);
+
 		foreach ($data as $key => $val) {
-			$val = strval($val);
-			if ($key != '' && strpos($val, '@') !== 0)
+			if (is_array($val) || is_object($val)) {
+				$val = json_encode($val, JSON_UNESCAPED_UNICODE);
+			} else {
+				$val = strval($val);
+			}
+
+			if ($key != '' && strpos($val, '@') !== 0) {
 				$signStr .= $key . $val;
+			}
 		}
+
 		$data['_sign'] = strtoupper(md5($signStr . $appSecret));
 
 		return $data;
@@ -215,9 +224,9 @@ class Encrypt
 
 	/**
 	 * Desc: 检验签名是否有效
-	 * @param array $data  数据
+	 * @param array $data 数据
 	 * @param string $appSecret 秘钥
-	 * @param int $timeDiff  允许的时间差
+	 * @param int $timeDiff 允许的时间差
 	 * @return bool
 	 * User: 青山有木
 	 * Date: 2023/8/15
@@ -225,23 +234,37 @@ class Encrypt
 	 */
 	public static function checkVerify(array $data, string $appSecret, $timeDiff = 300)
 	{
-		if (!isset($data['_timestamp']) || !isset($data['_sign']))
+		if (!isset($data['_timestamp']) || !isset($data['_sign'])){
 			return false; //Arguments missing
+		}
+
 		$timestamp = is_numeric($data['_timestamp']) ? $data['_timestamp'] : strtotime($data['_timestamp']);
-		if ($timestamp < (time() - $timeDiff) || $timestamp > (time() + $timeDiff))
+		if ($timestamp < (time() - $timeDiff) || $timestamp > (time() + $timeDiff)){
 			return false; //Invalid timestamp
+		}
+
 		$originSign = $data['_sign'];
 		unset($data['_sign']);
 		ksort($data);
 		$signStr = $appSecret;
+
 		foreach ($data as $key => $val) {
-			$val = strval($val);
-			if ($key != '' && strpos($val, '@') !== 0)
+			if (is_array($val) || is_object($val)) {
+				$val = json_encode($val, JSON_UNESCAPED_UNICODE);
+			} else {
+				$val = strval($val);
+			}
+
+			if ($key != '' && strpos($val, '@') !== 0) {
 				$signStr .= $key . $val;
+			}
 		}
+
 		$sign = strtoupper(md5($signStr . $appSecret));
-		if ($sign !== $originSign)
+		if ($sign !== $originSign){
 			return false; //Signature verification failed
+		}
+
 		return true;
 	}
 
